40dedf1b9e5048a1749e6890eb3f9ef332d365c6
krss11 <139711215+krss11@users.noreply.github.com>
2023-10-30 16:12:06 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,6 +2,6 @@ provider "azurerm" {
   features {}
 client_id = "98802061-a5ad-41c4-b8f5-57129928fe98"
 client_secret = "YqI8Q~6dKJq7olh5Xtyh4c4ihVpCUjAEqCSt5ctx"
-scbscription_id = "9cf5d1f1-a8b1-40f8-b4bf-f085e769152b"
+subscription_id = "9cf5d1f1-a8b1-40f8-b4bf-f085e769152b"
 tenent_it = "878aab2f-ad44-4911-b939-54d57cc4491f"
 }
