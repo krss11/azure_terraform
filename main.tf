8f751d8e518de0c25f83963a6692a943fd07996b
krss11 <139711215+krss11@users.noreply.github.com>
2023-11-05 00:06:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,35 @@
-resource "azurerm_resource_group" "aks-rg" {
-  name     = var.resource_group_name
-  location = var.location
+provider "azurerm" {
+  features {}
 }
-
-resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.aks-rg.name
-  location            = var.location
-  sku                 = "Standard"
-  admin_enabled       = false
+resource "azurerm_resource_group" "aks-rg" {
+  name     = "myResourceGroup"
+  location = "East US"
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.cluster_name
-  kubernetes_version  = var.kubernetes_version
-  location            = var.location
+  name                = "myAKSCluster"
+  location            = azurerm_resource_group.aks-rg.location
   resource_group_name = azurerm_resource_group.aks-rg.name
-  dns_prefix          = var.cluster_name
+  dns_prefix          = "myaksdns"
 
   default_node_pool {
-    name                = "system"
-    node_count          = var.system_node_count
+    name                = "default"
+    node_count          = 1
     vm_size             = "Standard_DS2_v2"
-    type                = "VirtualMachineScaleSets"
-    zones  = [1, 2, 3]
-    enable_auto_scaling = false
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  network_profile {
-    load_balancer_sku = "standard"
-    network_plugin    = "kubenet" 
+  tags = {
+    environment = "dev"
   }
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_config[0].kube_config
+}
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
 }
