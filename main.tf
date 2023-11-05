dd098450d4ad04c14d93ac7753119cafb9dcdfa7
krss11 <139711215+krss11@users.noreply.github.com>
2023-11-05 18:00:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   #}
   service_principal  {
     client_id = "24b84c27-b06f-496e-a5c4-06ea8150ac2f"
-    client_secret = "iQi8Q~QjvL.~dguV~kuBaRTUl1U82hF-UZY8HdrV"
+    client_secret = "qh38Q~ueKz6VPJzC4woUT-S7PmXonG0nrZGcsbh0"
 
  }
 
