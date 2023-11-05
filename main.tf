b99d265477eae02d96dcdaefa1a9e08ff08772c5
krss11 <139711215+krss11@users.noreply.github.com>
2023-11-05 11:04:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,14 +17,14 @@ resource "azurerm_kubernetes_cluster" "aks" {
     node_count          = 1
     vm_size             = "Standard_DS2_v2"
   }
-  #identity {
-  #  type = "SystemAssigned"
-  #}
-  service_principal  {
-    client_id = "04ee7578-aff2-4cc1-8886-db4d3363ee5d"
-    client_secret = "oX68Q~ghruRuuByQxY2NfRA6PqSG3Dym6iaX7cxT"
-    
+  identity {
+    type = "SystemAssigned"
   }
+  #service_principal  {
+  #  client_id = "04ee7578-aff2-4cc1-8886-db4d3363ee5d"
+  #  client_secret = "oX68Q~ghruRuuByQxY2NfRA6PqSG3Dym6iaX7cxT"
+    
+ # }
 
   tags = {
     environment = "dev"
