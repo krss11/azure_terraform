9021bd7c12f35ac5be4de109b04cebc245e08fc2
krss11 <139711215+krss11@users.noreply.github.com>
2023-11-05 07:52:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,8 +16,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   service_principal  {
-    client_id = "98802061-a5ad-41c4-b8f5-57129928fe98"
-    client_secret = "YqI8Q~6dKJq7olh5Xtyh4c4ihVpCUjAEqCSt5ctx"
+    ARM_CLIENT_ID = "98802061-a5ad-41c4-b8f5-57129928fe98"
+    ARM_CLIENT_SECRET = "YqI8Q~6dKJq7olh5Xtyh4c4ihVpCUjAEqCSt5ctx"
+    ARM_SUBSCRIPTION_ID = "9cf5d1f1-a8b1-40f8-b4bf-f085e769152b"
+    ARM_TENANT_ID = "878aab2f-ad44-4911-b939-54d57cc4491f"
   }
 
   tags = {
